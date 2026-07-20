168f3f4c31186d3b2ecc6e71d3129d3e8ce2f418
Thamizh Learner <NaveenLearnsThamizh@gmail.com>
2026-07-20 21:45:08 +0530

    Work on kural analysis

--- activityKural.go
+++ activityKural.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
 	"strings"
 
 	script "github.com/ThamizhLearner/Thamizh"
+	kural2 "github.com/ThamizhLearner/ThamizhLocalhost/kural"
 )
 
 type kuralActivity struct{}
@@ -23,10 +25,12 @@ func (a kuralActivity) Respond(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, seed)
 }
 
+// Kural display source
 type kural struct {
 	Num         int
 	Line, Line2 string
 	Syl, Syl2   string
+	RhythmTable SimpleTable
 }
 
 func LoadAllKurals(fn string) []kural {
@@ -40,14 +44,20 @@ func LoadAllKurals(fn string) []kural {
 	var kurals []kural
 	num := 1
 	for scanner.Scan() {
-		line := scanner.Text()
+		line := strings.Trim(scanner.Text(), " ")
 		if !scanner.Scan() {
 			panic("Unexpected EOF")
 		}
-		line2 := scanner.Text()
+		line2 := strings.Trim(scanner.Text(), " ")
 		syl, syl2 := SyllabifyLong(line), SyllabifyLong(line2)
-		kurals = append(kurals, kural{num, line, line2, syl, syl2})
+		kuralStr := line + " " + line2
+		kurals = append(kurals, kural{num, line, line2, syl, syl2, createThirukkuralRythmTable(kuralStr)})
 		num++
+
+		// Just handle first 10 kurals!
+		if num == 11 {
+			break
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -56,6 +66,7 @@ func LoadAllKurals(fn string) []kural {
 	return kurals
 }
 
+// Syllabifies spaced string!
 func SyllabifyLong(str string) string {
 	strs := strings.Split(str, " ")
 	for i, ustr := range strs {
@@ -66,4 +77,43 @@ func SyllabifyLong(str string) string {
 		strs[i], _ = script.SyllabifiedUStr(str, "-")
 	}
 	return strings.Join(strs, " ")
+}
+
+var rhythmBaseMap = kural2.GetRhythmBaseMap()
+
+func createThirukkuralRythmTable(k string) SimpleTable {
+	var t = SimpleTable{
+		Title:       "அலகிட்டு வாய்ப்பாடு",
+		ColInfoList: []ColInfo{{"சீர்", 1}, {"அசை", 1}, {"வாய்ப்பாடு", 1}},
+		Cells:       make([][]string, 7),
+	}
+	// fmt.Println(k)
+	strs := strings.Split(k, " ")
+	if len(strs) != 7 {
+		fmt.Println(k)
+		panic("Expected 7 சீர்கள்")
+	}
+	for i, str := range strs {
+		if i == 7 {
+			break
+		}
+		row := make([]string, 3)
+		t.Cells[i] = row
+		ok := !strings.Contains(str, "ஃ")
+		if !ok {
+			row[0] = str
+			row[1] = "Failed"
+			row[2] = "Failed"
+			continue
+		}
+		ls := script.MustLetterSeqFrom(str)
+		captures := kural2.CaptureRhythm(ls, i == 6)
+		key := kural2.CreateKey(captures)
+		rhythm := rhythmBaseMap[key]
+		row[0] = str
+		row[1] = kural2.GetRhythmBeats(captures)
+		row[2] = rhythm.Value
+	}
+
+	return t
 }
