2156cbe5c5f687c6ddc947ba8900622f1879a2b2
Thamizh Learner <NaveenLearnsThamizh@gmail.com>
2026-07-25 17:51:56 +0530

    Added வாய்ப்பாடு table, நேர், நிரை definition reference (#44)

--- activityMisc.go
+++ activityMisc.go
@@ -11,9 +11,12 @@ func (a miscActivity) GetID() string   { return "Misc" }
 func (a miscActivity) GetDesc() string { return "Uncategorized cache" }
 func (a miscActivity) Respond(w http.ResponseWriter, r *http.Request) {
 	seed := struct {
-		InfoTable SimpleTable
-		VerbGraph string
-	}{finalTable2(), createVerbGraph()}
+		InfoTable   SimpleTable
+		NerTable    SimpleTable
+		NiraiTable  SimpleTable
+		RhythmTable SimpleTable
+		VerbGraph   string
+	}{finalTable2(), createNerTable(), createNiraiTable(), createRhythmTable(), createVerbGraph()}
 
 	var tmpl = template.Must(template.ParseFiles("tmpls/index.tmpl", "tmpls/misc.tmpl"))
 	tmpl.Execute(w, seed)
