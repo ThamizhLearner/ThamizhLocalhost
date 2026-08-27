b54e5581e72b2603dd9bff917a3475a6ff7940a9
Thamizh Learner <NaveenLearnsThamizh@gmail.com>
2026-08-27 18:25:18 +0530

    Server is now stateless

--- activity.go
+++ activity.go
@@ -23,19 +23,13 @@ var activities []Activity = []Activity{
 	kuralActivity{},
 }
 
-// Currently active activity
-var currentActivity = activities[0]
-
 func getDefaultActivity() Activity { return activities[0] }
-func getCurrentActivity() Activity { return currentActivity }
 
 func selectActivityById(id string) Activity {
-	// Note: We only update the current activity, if the activity name matches!
 	for _, a := range activities {
 		if a.GetID() == id {
-			currentActivity = a
-			break
+			return a
 		}
 	}
-	return currentActivity
+	return getDefaultActivity()
 }
