7cb4a05a169be9156658afd7ee8d0ee3ea4b562e
Thamizh Learner <NaveenLearnsThamizh@gmail.com>
2026-08-28 20:10:26 +0530

    Chatter mode to track GET/POST requests

--- main.go
+++ main.go
@@ -7,11 +7,19 @@ import (
 	"strings"
 )
 
+// Log GET/POST requests...
+var chatterOn bool = false
+
 func main() {
 	var addr = "localhost:8080"
 	if len(os.Args) == 2 {
 		if os.Args[1] == "Host@Render" {
+			port := os.Getenv("PORT")
+			fmt.Printf("%s requested using port %s\n", os.Args[1], port)
 			addr = "0.0.0.0:10000"
+
+			// Chatter mode!
+			chatterOn = os.Getenv("Chatter") == "1"
 		}
 	}
 	setupServer()
@@ -36,18 +44,38 @@ func setupServer() {
 	http.HandleFunc("GET /", defaultActivityPresenter)
 	http.HandleFunc("POST /{activity}", activityPresenter)
 	http.HandleFunc("GET /ping", pingResponder)
+	http.HandleFunc("GET /favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
 	http.HandleFunc("GET /{activity}", activityPresenter)
 	fs := http.FileServer(http.Dir("style"))
 	http.Handle("GET /style.css", fs)
 }
 
 func defaultActivityPresenter(w http.ResponseWriter, r *http.Request) {
+	if chatterOn {
+		fmt.Println("GET Default")
+	}
 	activity := getDefaultActivity()
 	activity.Respond(w, r)
 }
 
 func activityPresenter(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("activity")
+
+	if chatterOn {
+		if r.Method == http.MethodPost {
+			if id == "SylWord" || id == "Decomposition" {
+				query := r.FormValue("inpStr")
+				fmt.Printf("POST %s; %s\n", id, query)
+			} else {
+				fmt.Printf("POST %s\n", id)
+			}
+		} else {
+			fmt.Printf("GET %s\n", id)
+		}
+	}
+
 	activity := selectActivityById(id)
 	activity.Respond(w, r)
 }
